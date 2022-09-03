632ee40e53e5fdcc15b42f6d2b4b3c1b807724c6
henthiran <kenthiran06@gmail.com>
2022-09-03 13:15:58 +0530

    first commit

---
+++ Employee Salary Detail/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Employee_Salary_Detail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEmpno = new System.Windows.Forms.Label();
+            this.lblEmpName = new System.Windows.Forms.Label();
+            this.lblBsalary = new System.Windows.Forms.Label();
+            this.lblAllowance = new System.Windows.Forms.Label();
+            this.lblDesignation = new System.Windows.Forms.Label();
+            this.txtEMPNo = new System.Windows.Forms.TextBox();
+            this.txtEmpname = new System.Windows.Forms.TextBox();
+            this.txtBSalary = new System.Windows.Forms.TextBox();
+            this.txtAllowance = new System.Windows.Forms.TextBox();
+            this.cmbDesignation = new System.Windows.Forms.ComboBox();
+            this.lblEPF = new System.Windows.Forms.Label();
+            this.lblNsalary = new System.Windows.Forms.Label();
+            this.lblGsallary = new System.Windows.Forms.Label();
+            this.lblEPFBasalary = new System.Windows.Forms.Label();
+            this.lblGrossSalary = new System.Windows.Forms.Label();
+            this.lblNetSalary = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblEmpno
+            // 
+            this.lblEmpno.AutoSize = true;
+            this.lblEmpno.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmpno.Location = new System.Drawing.Point(33, 21);
+            this.lblEmpno.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEmpno.Name = "lblEmpno";
+            this.lblEmpno.Size = new System.Drawing.Size(92, 17);
+            this.lblEmpno.TabIndex = 0;
+            this.lblEmpno.Text = "Employee No";
+            // 
+            // lblEmpName
+            // 
+            this.lblEmpName.AutoSize = true;
+            this.lblEmpName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmpName.Location = new System.Drawing.Point(33, 58);
+            this.lblEmpName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEmpName.Name = "lblEmpName";
+            this.lblEmpName.Size = new System.Drawing.Size(111, 17);
+            this.lblEmpName.TabIndex = 1;
+            this.lblEmpName.Text = "Employee Name";
+            // 
+            // lblBsalary
+            // 
+            this.lblBsalary.AutoSize = true;
+            this.lblBsalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBsalary.Location = new System.Drawing.Point(33, 89);
+            this.lblBsalary.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblBsalary.Name = "lblBsalary";
+            this.lblBsalary.Size = new System.Drawing.Size(86, 17);
+            this.lblBsalary.TabIndex = 2;
+            this.lblBsalary.Text = "Basic Salary";
+            // 
+            // lblAllowance
+            // 
+            this.lblAllowance.AutoSize = true;
+            this.lblAllowance.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAllowance.Location = new System.Drawing.Point(33, 119);
+            this.lblAllowance.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAllowance.Name = "lblAllowance";
+            this.lblAllowance.Size = new System.Drawing.Size(71, 17);
+            this.lblAllowance.TabIndex = 3;
+            this.lblAllowance.Text = "Allowance";
+            // 
+            // lblDesignation
+            // 
+            this.lblDesignation.AutoSize = true;
+            this.lblDesignation.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDesignation.Location = new System.Drawing.Point(33, 155);
+            this.lblDesignation.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDesignation.Name = "lblDesignation";
+            this.lblDesignation.Size = new System.Drawing.Size(83, 17);
+            this.lblDesignation.TabIndex = 4;
+            this.lblDesignation.Text = "Designation";
+            // 
+            // txtEMPNo
+            // 
+            this.txtEMPNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEMPNo.Location = new System.Drawing.Point(190, 18);
+            this.txtEMPNo.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEMPNo.Name = "txtEMPNo";
+            this.txtEMPNo.Size = new System.Drawing.Size(159, 23);
+            this.txtEMPNo.TabIndex = 5;
+            // 
+            // txtEmpname
+            // 
+            this.txtEmpname.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmpname.Location = new System.Drawing.Point(190, 53);
+            this.txtEmpname.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEmpname.Name = "txtEmpname";
+            this.txtEmpname.Size = new System.Drawing.Size(242, 23);
+            this.txtEmpname.TabIndex = 6;
+            // 
+            // txtBSalary
+            // 
+            this.txtBSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBSalary.Location = new System.Drawing.Point(190, 84);
+            this.txtBSalary.Margin = new System.Windows.Forms.Padding(2);
+            this.txtBSalary.Name = "txtBSalary";
+            this.txtBSalary.Size = new System.Drawing.Size(129, 23);
+            this.txtBSalary.TabIndex = 7;
+            // 
+            // txtAllowance
+            // 
+            this.txtAllowance.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAllowance.Location = new System.Drawing.Point(190, 115);
+            this.txtAllowance.Margin = new System.Windows.Forms.Padding(2);
+            this.txtAllowance.Name = "txtAllowance";
+            this.txtAllowance.Size = new System.Drawing.Size(129, 23);
+            this.txtAllowance.TabIndex = 8;
+            // 
+            // cmbDesignation
+            // 
+            this.cmbDesignation.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbDesignation.FormattingEnabled = true;
+            this.cmbDesignation.Items.AddRange(new object[] {
+            "Manager",
+            "Accountant",
+            "Clerk"});
+            this.cmbDesignation.Location = new System.Drawing.Point(194, 153);
+            this.cmbDesignation.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbDesignation.Name = "cmbDesignation";
+            this.cmbDesignation.Size = new System.Drawing.Size(154, 24);
+            this.cmbDesignation.TabIndex = 9;
+            this.cmbDesignation.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // lblEPF
+            // 
+            this.lblEPF.AutoSize = true;
+            this.lblEPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEPF.Location = new System.Drawing.Point(28, 22);
+            this.lblEPF.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEPF.Name = "lblEPF";
+            this.lblEPF.Size = new System.Drawing.Size(190, 17);
+            this.lblEPF.TabIndex = 10;
+            this.lblEPF.Text = "EPF (8% of Basic Salary)";
+            // 
+            // lblNsalary
+            // 
+            this.lblNsalary.AutoSize = true;
+            this.lblNsalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNsalary.Location = new System.Drawing.Point(28, 73);
+            this.lblNsalary.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNsalary.Name = "lblNsalary";
+            this.lblNsalary.Size = new System.Drawing.Size(82, 17);
+            this.lblNsalary.TabIndex = 11;
+            this.lblNsalary.Text = "Net salary";
+            // 
+            // lblGsallary
+            // 
+            this.lblGsallary.AutoSize = true;
+            this.lblGsallary.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGsallary.Location = new System.Drawing.Point(28, 45);
+            this.lblGsallary.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblGsallary.Name = "lblGsallary";
+            this.lblGsallary.Size = new System.Drawing.Size(102, 17);
+            this.lblGsallary.TabIndex = 12;
+            this.lblGsallary.Text = "Gross Salary";
+            // 
+            // lblEPFBasalary
+            // 
+            this.lblEPFBasalary.AutoSize = true;
+            this.lblEPFBasalary.Location = new System.Drawing.Point(293, 22);
+            this.lblEPFBasalary.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEPFBasalary.Name = "lblEPFBasalary";
+            this.lblEPFBasalary.Size = new System.Drawing.Size(20, 17);
+            this.lblEPFBasalary.TabIndex = 13;
+            this.lblEPFBasalary.Text = "--";
+            // 
+            // lblGrossSalary
+            // 
+            this.lblGrossSalary.AutoSize = true;
+            this.lblGrossSalary.Location = new System.Drawing.Point(293, 45);
+            this.lblGrossSalary.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblGrossSalary.Name = "lblGrossSalary";
+            this.lblGrossSalary.Size = new System.Drawing.Size(20, 17);
+            this.lblGrossSalary.TabIndex = 14;
+            this.lblGrossSalary.Text = "--";
+            // 
+            // lblNetSalary
+            // 
+            this.lblNetSalary.AutoSize = true;
+            this.lblNetSalary.Location = new System.Drawing.Point(293, 73);
+            this.lblNetSalary.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNetSalary.Name = "lblNetSalary";
+            this.lblNetSalary.Size = new System.Drawing.Size(20, 17);
+            this.lblNetSalary.TabIndex = 15;
+            this.lblNetSalary.Text = "--";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblNetSalary);
+            this.groupBox1.Controls.Add(this.lblGrossSalary);
+            this.groupBox1.Controls.Add(this.lblEPFBasalary);
+            this.groupBox1.Controls.Add(this.lblGsallary);
+            this.groupBox1.Controls.Add(this.lblNsalary);
+            this.groupBox1.Controls.Add(this.lblEPF);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(36, 190);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(506, 112);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Salary Details";
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnProcess.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcess.Location = new System.Drawing.Point(205, 318);
+            this.btnProcess.Margin = new System.Windows.Forms.Padding(2);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(143, 37);
+            this.btnProcess.TabIndex = 17;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(399, 318);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(143, 37);
+            this.btnClear.TabIndex = 18;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cmbDesignation);
+            this.Controls.Add(this.txtAllowance);
+            this.Controls.Add(this.txtBSalary);
+            this.Controls.Add(this.txtEmpname);
+            this.Controls.Add(this.txtEMPNo);
+            this.Controls.Add(this.lblDesignation);
+            this.Controls.Add(this.lblAllowance);
+            this.Controls.Add(this.lblBsalary);
+            this.Controls.Add(this.lblEmpName);
+            this.Controls.Add(this.lblEmpno);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Employee Salary Detail";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEmpno;
+        private System.Windows.Forms.Label lblEmpName;
+        private System.Windows.Forms.Label lblBsalary;
+        private System.Windows.Forms.Label lblAllowance;
+        private System.Windows.Forms.Label lblDesignation;
+        private System.Windows.Forms.TextBox txtEMPNo;
+        private System.Windows.Forms.TextBox txtEmpname;
+        private System.Windows.Forms.TextBox txtBSalary;
+        private System.Windows.Forms.TextBox txtAllowance;
+        private System.Windows.Forms.ComboBox cmbDesignation;
+        private System.Windows.Forms.Label lblEPF;
+        private System.Windows.Forms.Label lblNsalary;
+        private System.Windows.Forms.Label lblGsallary;
+        private System.Windows.Forms.Label lblEPFBasalary;
+        private System.Windows.Forms.Label lblGrossSalary;
+        private System.Windows.Forms.Label lblNetSalary;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Employee Salary Detail/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Employee_Salary_Detail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            double BS, Alo, EPF, GS, NS;
+
+            BS = double.Parse(txtBSalary.Text);
+            Alo = double.Parse(txtAllowance.Text);
+
+            EPF = (BS / 100) * 8;
+
+            lblEPFBasalary.Text = EPF.ToString();
+
+            GS = BS + Alo;
+            lblGrossSalary.Text = GS.ToString();
+
+            NS = GS - EPF;
+            lblNetSalary.Text = NS.ToString();
+        
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtAllowance.Clear();
+            txtEMPNo.Clear();
+            txtEmpname.Clear();
+            txtBSalary.Clear();
+            lblEPFBasalary.Text = "";
+            lblGrossSalary.Text = "";
+            lblNetSalary.Text = "";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
